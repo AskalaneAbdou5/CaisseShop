a2fcb9682498d1299b1bf88e39aa41b686e698f2
AskalaneAbdou5 <askalane.abdou5369@gmail.com>
2026-04-02 09:43:18 +0200

    ajout de la fonctionnalité insert de produit

---
+++ frontoffice/insert.php
@@ -0,0 +1,40 @@
+<?php
+require_once('../asset/connexionbdd.php');
+
+
+if (isset($_POST['nouveauNomProduit']) 
+    && isset($_POST['nouveauPrixProduit']) 
+    && isset($_POST['nouveauDescripProduit'])
+    && isset($_POST['quantiteNouveauProduit'])
+    && isset($_POST['codeBarNouveauProduit'])) {
+
+    $nom=trim($_POST['nouveauNomProduit']);
+    $prix=trim($_POST['nouveauPrixProduit']);
+    $descrip=trim($_POST['nouveauDescripProduit']);
+    $quantite=trim($_POST['quantiteNouveauProduit']);
+    $codebar=trim($_POST['codeBarNouveauProduit']);
+
+   
+    if (!empty($nom) && !empty($prix) && !empty($descrip) && !empty($quantite) && !empty($codebar)) {
+
+
+        $sql = "INSERT INTO produits(NomProduit,Description,Prix,Stock,CodeBarres) VALUES(:nom, :descrip, :prix, :quantite, :codebar)"; 
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([
+            'nom' => $nom,
+            'prix' => $prix,
+            'descrip' => $descrip,
+            'quantite' => $quantite,
+            'codebar' => $codebar,
+        ]);
+
+        echo "<script> alert('Le produit a été bien ajouter');</script>";
+    }else{
+        echo "<script> alert('Veuillez remplir tous les champs');
+        window.location.href = 'contact.php';</script>";
+    }
+    
+}
+
+
+?>
