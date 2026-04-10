f7eda957e8c304a5cde4b80a10f6729632bca2ee
AskalaneAbdou5 <askalane.abdou5369@gmail.com>
2026-04-10 09:27:07 +0200

    modif sur la base de données

--- caisseshop.sql
+++ caisseshop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : mar. 31 mars 2026 à 12:09
+-- Généré le : ven. 10 avr. 2026 à 09:25
 -- Version du serveur : 10.4.32-MariaDB
 -- Version de PHP : 8.2.12
 
@@ -41,14 +41,15 @@ CREATE TABLE `produits` (
 --
 
 INSERT INTO `produits` (`Id`, `NomProduit`, `Description`, `Prix`, `Stock`, `CodeBarres`) VALUES
-(1, 'Pain', 'Pain bio 400 g', 2.5, 200, '12252001'),
-(2, 'Pain Luxe', 'Pain artisanal truffé 400 g', 25, 50, '12252002'),
+(1, 'Pain', 'Pain bio 400 g', 2.5, 187, '12252002'),
+(2, 'Pain Luxe', 'Pain artisanal truffé 400 g', 25, 49, '12252006'),
 (3, 'Fromage Premium', 'Fromage affiné 24 mois', 120, 30, '12252003'),
 (4, 'Caviar Royal', 'Caviar importé haute qualité 100 g', 450, 10, '12252004'),
 (5, 'Huile d’olive rare', 'Huile d’olive extra vierge édition limitée', 85, 40, '12252005'),
-(6, 'Chocolat Gold', 'Chocolat avec feuille d’or 24 carats', 150, 25, '12252006'),
+(6, 'Chocolat Gold', 'Chocolat avec feuille d’or 24 carats', 150, 25, '12252009'),
 (7, 'Miel Sauvage', 'Miel rare récolté en montagne', 95, 35, '12252007'),
-(8, 'Café Prestige', 'Café grains sélection spéciale', 60, 60, '12252008');
+(8, 'Café Prestige', 'Café grains sélection spéciale', 60, 60, '12252008'),
+(17, '100 KG de RIZ', 'yuyujyuj', 33, 2000, '122520018');
 
 -- --------------------------------------------------------
 
@@ -69,7 +70,7 @@ CREATE TABLE `utilisateurs` (
 --
 
 INSERT INTO `utilisateurs` (`Id`, `Nom`, `Prenom`, `Email`, `MotDePasse`) VALUES
-(1, 'Mamadou', 'abdou', 'mamadou@gmail.com', 'mamadou.com'),
+(1, 'Mamadou', 'abdou', 'mamadou@gmail.com', '$2y$10$Hp3dyoJ16KULuhqULZhwnuiTyx1Vmc53hcS7fXBKjZI8WpA6Zq/JS'),
 (2, 'Patate', 'Jean', 'jean.patate@gmail.com', 'patate123'),
 (3, 'Fromage', 'Camille', 'camille.fromage@gmail.com', 'cheese456'),
 (4, 'Baguette', 'Pierre', 'pierre.baguette@gmail.com', 'pain789'),
@@ -100,17 +101,22 @@ CREATE TABLE `venteproduits` (
 --
 
 INSERT INTO `venteproduits` (`Id`, `IdVente`, `IdProduit`, `Quantite`, `PrixTotalProduit`) VALUES
-(1, 2, 4, 6, 9000.2),
+(1, 2, 4, 6, 2700),
 (2, 3, 1, 2, 5),
-(3, 3, 2, 1, 120),
-(4, 4, 3, 5, 2250),
-(5, 5, 4, 3, 269.97),
-(6, 6, 5, 6, 190),
-(7, 7, 6, 7, 600),
-(8, 8, 2, 1, 10),
+(3, 11, 2, 1, 25),
+(4, 4, 3, 5, 600),
+(5, 5, 4, 3, 1350),
+(6, 6, 5, 6, 510),
+(7, 7, 6, 7, 1050),
+(8, 8, 2, 1, 25),
 (9, 9, 3, 2, 240),
-(10, 10, 4, 5, 150),
-(11, 2, 7, 3, 719.92);
+(10, 10, 4, 5, 2250),
+(11, 1, 7, 3, 285),
+(13, 1, 1, 3, 7.5),
+(14, 1, 3, 3, 360),
+(15, 12, 1, 1, 2.5),
+(16, 12, 2, 1, 25),
+(17, 13, 1, 12, 30);
 
 -- --------------------------------------------------------
 
@@ -130,17 +136,19 @@ CREATE TABLE `ventes` (
 --
 
 INSERT INTO `ventes` (`Id`, `IdUtilisateur`, `Date`, `Total`) VALUES
-(1, 1, '2026-03-10', 2055.62),
-(2, 2, '2026-03-11', 150.75),
-(3, 3, '2026-03-12', 3200),
-(4, 4, '2026-03-13', 89.99),
-(5, 5, '2026-03-14', 540.3),
-(6, 6, '2026-03-15', 9999.99),
-(7, 7, '2026-03-16', 45.2),
-(8, 8, '2026-03-17', 780.6),
-(9, 9, '2026-03-18', 120),
-(10, 10, '2026-03-19', 2500.45),
-(11, 1, '2026-03-20', 670.1);
+(1, 1, '2026-03-10', 652.5),
+(2, 2, '2026-03-11', 2700),
+(3, 3, '2026-03-12', 5),
+(4, 4, '2026-03-13', 600),
+(5, 5, '2026-03-14', 1350),
+(6, 6, '2026-03-15', 510),
+(7, 7, '2026-03-16', 1050),
+(8, 8, '2026-03-17', 25),
+(9, 9, '2026-03-18', 240),
+(10, 10, '2026-03-19', 2250),
+(11, 1, '2026-03-20', 25),
+(12, 1, '2026-04-09', 27.5),
+(13, 1, '2026-04-09', 30);
 
 --
 -- Index pour les tables déchargées
@@ -181,7 +189,7 @@ ALTER TABLE `ventes`
 -- AUTO_INCREMENT pour la table `produits`
 --
 ALTER TABLE `produits`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT pour la table `utilisateurs`
@@ -193,13 +201,13 @@ ALTER TABLE `utilisateurs`
 -- AUTO_INCREMENT pour la table `venteproduits`
 --
 ALTER TABLE `venteproduits`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT pour la table `ventes`
 --
 ALTER TABLE `ventes`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- Contraintes pour les tables déchargées
@@ -209,14 +217,13 @@ ALTER TABLE `ventes`
 -- Contraintes pour la table `venteproduits`
 --
 ALTER TABLE `venteproduits`
-  ADD CONSTRAINT `produi_venteproduit` FOREIGN KEY (`IdProduit`) REFERENCES `produits` (`Id`),
-  ADD CONSTRAINT `vente_venteproduit` FOREIGN KEY (`IdVente`) REFERENCES `ventes` (`Id`);
+  ADD CONSTRAINT `produi_venteproduit` FOREIGN KEY (`IdProduit`) REFERENCES `produits` (`Id`) ON DELETE NO ACTION;
 
 --
 -- Contraintes pour la table `ventes`
 --
 ALTER TABLE `ventes`
-  ADD CONSTRAINT `utilisateur_vente` FOREIGN KEY (`IdUtilisateur`) REFERENCES `utilisateurs` (`Id`);
+  ADD CONSTRAINT `utilisateur_vente` FOREIGN KEY (`IdUtilisateur`) REFERENCES `utilisateurs` (`Id`) ON DELETE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
